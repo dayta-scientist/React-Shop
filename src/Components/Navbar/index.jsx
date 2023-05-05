da72daca074b43d7165073e15e14cb28da421362
Gabriel LR <dayta.scientist@gmail.com>
2023-05-05 10:47:58 -0300

    Opening the detail of each product and heroicons with TailwindCSS

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Navbar = () => {
@@ -100,8 +101,9 @@ const Navbar = () => {
             Sign In
           </NavLink>
         </li>
-        <li>
-          🛒 {context.count}
+        <li className='flex items-center'>
+        <ShoppingCartIcon className='h-6 w-6 text-black'/> 
+        <div>{context.count}</div>
         </li>
       </ul>
     </nav>
