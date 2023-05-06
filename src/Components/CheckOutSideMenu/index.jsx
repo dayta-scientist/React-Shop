df3af3ee2532625e03dc79211ec94c6171f6aa9f
Gabriel LR <dayta.scientist@gmail.com>
2023-05-06 14:10:55 -0300

    OrderCard component avoiding duplicate products in the cart

--- src/Components/CheckOutSideMenu/index.jsx
+++ src/Components/CheckOutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -16,6 +17,18 @@ const CheckoutSideMenu = () => {
             className='h-6 w-6 text-black cursor-pointer'
             onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
+      </div>
+      <div className='px-6 overflow-x-scroll'>
+      {
+        context.cartProducts.map(product => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            imageURL={product.images}
+            price={product.price}
+          />
+        ))
+      }
       </div>
     </aside>
   )
