ac0c990b746ad6a0d5bae14b44fd0e004a60efee
Gabriel LR <dayta.scientist@gmail.com>
2023-05-08 07:38:12 -0300

    Fixing app bugs

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,28 +1,28 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
-  //Shopping Cart - Increment quantity
+  // Shopping Cart · Increment quantity
   const [count, setCount] = useState(0)
-  
-  //Product Detail - Open/Close
+
+  // Product Detail · Open/Close
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
   const openProductDetail = () => setIsProductDetailOpen(true)
   const closeProductDetail = () => setIsProductDetailOpen(false)
 
-  //Product Detail - Show Product
-  const [productToShow, setProductToShow] = useState({})
-
-  //Shopping Cart - Add products to cart
-  const [cartProducts, setCartProducts] = useState([])
-
-  //Checkout Side Menu  - Open/Close
+  // Checkout Side Menu · Open/Close
   const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
   const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
   const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
 
-  // Shopping Cart - Order
+  // Product Detail · Show product
+  const [productToShow, setProductToShow] = useState({})
+
+  // Shopping Cart · Add products to cart
+  const [cartProducts, setCartProducts] = useState([])
+
+  // Shopping Cart · Order
   const [order, setOrder] = useState([])
 
   // Get products
@@ -31,6 +31,9 @@ export const ShoppingCartProvider = ({children}) => {
 
   // Get products by title
   const [searchByTitle, setSearchByTitle] = useState(null)
+
+  // Get products by category
+  const [searchByCategory, setSearchByCategory] = useState(null)
 
   useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
@@ -42,11 +45,36 @@ export const ShoppingCartProvider = ({children}) => {
     return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
   }
 
-  useEffect(() => {
-    if(searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
-  }, [items, searchByTitle])
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
 
-  return(
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
+
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
+
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
+  }
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])
+
+  return (
     <ShoppingCartContext.Provider value={{
       count,
       setCount,
@@ -66,9 +94,12 @@ export const ShoppingCartProvider = ({children}) => {
       setItems,
       searchByTitle,
       setSearchByTitle,
-      filteredItems
+      filteredItems,
+      searchByCategory,
+      setSearchByCategory
     }}>
       {children}
     </ShoppingCartContext.Provider>
   )
 }
+
